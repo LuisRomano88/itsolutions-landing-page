74bacfa6569cf98a631c5dd37173600a75207836
Luis Romano <goliari13@gmail.com>
2024-03-14 16:18:54 -0300

    termino con el texto que cambia y comienzo carrousel

--- itsolutions/src/app/nav-bar/nav-bar.component.ts
+++ itsolutions/src/app/nav-bar/nav-bar.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
 
+  words: string[] = ['HOGAR', 'NEGOCIO', 'EMPRESA'];
+  wordWrapperContent: string = '';
+  addingWord: boolean = false;
+  counter: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      if (this.wordWrapperContent.length > 0 && !this.addingWord) {
+        this.wordWrapperContent = this.wordWrapperContent.slice(0, -1);
+      } else {
+        this.addingWord = true;
+      }
+
+      if (this.addingWord) {
+        if (this.wordWrapperContent.length < this.words[this.counter].length) {
+          this.wordWrapperContent = this.words[this.counter].slice(0, this.wordWrapperContent.length + 1);
+        } else {
+          if (this.counter < this.words.length - 1) {
+            this.counter++;
+          } else {
+            this.counter = 0;
+          }
+          this.addingWord = false;
+        }
+      }
+    }, 250);
+  }
 }
