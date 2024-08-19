9a9e02ee4436755a9d0a5e73f2ce84dde8ec1c87
Luis Romano <goliari13@gmail.com>
2024-08-19 18:20:42 -0300

    actualizo vista login, registro y recuperar contraseña. Vistas responsive

--- itsolutions/src/app/components/login/login.component.ts
+++ itsolutions/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { th } from '@faker-js/faker';
 
 @Component({
   selector: 'app-login',
@@ -12,26 +13,24 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-
+  currentView: string = 'login';
   password: string = '';
   isPasswordVisible: boolean = false;
-  showRegisterForm: boolean = false;
-  changeLogin: boolean = true;
+  selectOption: boolean = false;
+
+  setView(view: string): void {
+    this.currentView = view;
+  }
 
 
   togglePasswordVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
-  
- 
 
-  toggleForm() {
-    this.showRegisterForm = !this.showRegisterForm;
+  toggleSelectOption(){
+    this.selectOption = !this.selectOption;
   }
 
-  toggleLogin(){
-    this.changeLogin = !this.changeLogin;
-  }
 
   onLoginSubmit() {
     // Lógica para manejar el login
@@ -42,4 +41,142 @@ export class LoginComponent implements OnInit {
     // Lógica para manejar el registro
     console.log('Register submit');
   }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+<div class="question">
+      <div class="question-head">
+        <img
+          class="img-logo"
+          src="../../../assets/img/MARCA_COLORPNG.png"
+          alt=""
+        />
+      </div>
+
+      <div class="question-body">
+        <div *ngIf="changeLogin; then register else login"></div>
+
+        <ng-template #register>
+          <p>
+            ¿Eres nuevo en el sitio?<br /><a
+              (click)="toggleForm()"
+              (click)="toggleLogin()"
+              >Registrate</a
+            >
+          </p>
+        </ng-template>
+
+        <ng-template #login>
+          <p>
+            Ya tengo una cuenta.<br /><a
+              (click)="toggleForm()"
+              (click)="toggleLogin()"
+              >Iniciar Sesión</a
+            >
+          </p>
+        </ng-template>
+        
+        <p><a (click)="toggleResetPassword()">Olvide la contraseña</a></p>
+      </div>
+    </div>
+
+    <ng-template #formLogin>
+      <div class="form-login" *ngIf="!showRegisterForm">
+        <div *ngIf="!changeGreet; then welcome"></div>
+        <ng-template #welcome>
+          <h4>Bienvenidos</h4>
+        </ng-template>
+
+        <form (ngSubmit)="onLoginSubmit()">
+          <label for="">Email</label>
+          <input type="email" name="email" id="email" />
+
+          <label for="contraseña">Contraseña</label>
+          <input
+            [type]="isPasswordVisible ? 'text' : 'password'"
+            class="password-input"
+            type="password"
+            name="password"
+            id="password"
+          />
+          <i id="icon-eye" (click)="togglePasswordVisibility()">
+            <img
+              [src]="
+                isPasswordVisible
+                  ? '../../../assets/img/eye-off.png'
+                  : '../../../assets/img/eye.png'
+              "
+              alt="Toggle Password"
+              alt=""
+            />
+          </i>
+
+          <button class="btn-iniciar">Iniciar Sesion</button>
+        </form>
+      </div>
+    </ng-template>
+
+    <!-- Formulario de Registro -->
+
+    <ng-template #registerForm>
+      <div class="form-register" >
+        <div *ngIf="!changeGreet; then regist"></div>
+        <ng-template #regist>
+          <h4>Registrarme</h4>
+        </ng-template>
+
+        <form (ngSubmit)="onRegisterSubmit()">
+          <!-- Campos de registro -->
+
+          <label for="name">Nombre</label>
+          <input id="name" name="name" type="text" required />
+
+          <label for="email">Email:</label>
+          <input id="email" name="email" type="email" required />
+
+          <label for="password">Contraseña:</label>
+          <input id="password" name="password" type="password" required />
+
+          <label for="re-password">Repetir Contraseña:</label>
+          <input id="re-password" name="re-password" type="password" required />
+
+          <button class="btn-iniciar" type="submit">Registrarme</button>
+        </form>
+      </div>
+    </ng-template>
+
+    <!-- Olvide la contraseña-->
+    <div *ngIf="resetPassword; then changePassword; else formLogin"></div>
+    <ng-template #changePassword>
+      <div class="reset-password">
+        <h4>Restaurar Contraseña</h4>
+        <p>¡Hola!</p>
+        <p>
+          ¿Olvidaste tu contraseña? No te preocupes, completa tu direccion de
+          correo y te enviamos un link para cambiarla.
+        </p>
+        <label for="email" id="email-forgot">Email</label>
+        <input type="email" id="email" />
+        <button class="btn-reset">Restaurar Contraseña</button>
+      </div>
+    </ng-template>
+  </div>
+
+  <!-- Renderiza el formulario de registro si showRegisterForm es true -->
+  <div *ngIf="showRegisterForm">
+    <ng-container *ngTemplateOutlet="registerForm"></ng-container>
+
+    */
